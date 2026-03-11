9c20931900423b7b6fa53933a30d26a2f1edd5e2
kantterosclothingco-lab <kantterosclothingco@gmail.com>
2026-03-11 05:29:32 -0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ fontFamily: "Arial, sans-serif", padding: "40px" }}>
-      <h1>Soulfood Fusion House</h1>
-      <p>New smart restaurant website coming soon.</p>
-    </main>
-  );
-}
