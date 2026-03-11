bdbcafc5915adc22c345516418d7f298fc41aaa4
kantterosclothingco-lab <kantterosclothingco@gmail.com>
2026-03-11 05:30:52 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "Arial, sans-serif", padding: "40px" }}>
+      <h1>Soulfood Fusion House</h1>
+      <p>New smart restaurant website coming soon.</p>
+    </main>
+  );
+}
