c4d8b5a0a55083baf3adb56fb974fed1730de18d
kantterosclothingco-lab <kantterosclothingco@gmail.com>
2026-03-28 23:34:33 -0700

    Update menu.js

--- pages/menu.js
+++ pages/menu.js
@@ -309,6 +309,8 @@ export default function MenuPage() {
       <Head>
         <title>Menu | Soulfood Fusion House</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
       </Head>
 
       <header className="topbar">
